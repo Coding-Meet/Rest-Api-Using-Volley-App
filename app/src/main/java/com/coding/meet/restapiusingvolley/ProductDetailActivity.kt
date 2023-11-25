e409a1aed49e1d4c3dbaaf6f0db3ac2cefca36e0
coding meet <codingmeet26@gmail.com>
2023-11-25 10:26:24 +0530

    Create Clickable Links in Text with Spannable String

--- app/src/main/java/com/coding/meet/restapiusingvolley/ProductDetailActivity.kt
+++ app/src/main/java/com/coding/meet/restapiusingvolley/ProductDetailActivity.kt
@@ -1,9 +1,15 @@
 package com.coding.meet.restapiusingvolley
 
+import android.content.Intent
+import android.graphics.Color
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.method.LinkMovementMethod
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.text.toSpannable
 import com.bumptech.glide.Glide
 import com.coding.meet.restapiusingvolley.models.Product
 import com.coding.meet.restapiusingvolley.models.Rating
@@ -37,5 +43,33 @@ class ProductDetailActivity : AppCompatActivity() {
         productPriceTxt.text = "USD ${product.price}"
         productRatingTxt.text = product.rating.rate.toString()
         productReviewTxt.text = "${product.rating.count} Reviews"
+
+
+        val spannableTxt = findViewById<TextView>(R.id.spannableTxt)
+        val strSpannable = spannableTxt.text.toSpannable() as SpannableString
+        strSpannable.withClickableSpan(
+            "Terms of Use",
+            Color.BLUE,
+            true,
+            onClickListener = {
+                // here Url pass
+                val termUrl = Uri.parse("https://github.com/Coding-Meet")
+                startActivity(Intent(Intent.ACTION_VIEW,termUrl))
+            }
+        )
+        strSpannable.withClickableSpan(
+            "Privacy Policy",
+            Color.BLUE,
+            true,
+            onClickListener = {
+                // here Url pass
+                val privacyUrl = Uri.parse("https://www.youtube.com/@CodingMeet26")
+                startActivity(Intent(Intent.ACTION_VIEW,privacyUrl))
+            }
+        )
+        spannableTxt.text = strSpannable
+        spannableTxt.movementMethod = LinkMovementMethod.getInstance()
+        spannableTxt.highlightColor = Color.TRANSPARENT
+
     }
 }
